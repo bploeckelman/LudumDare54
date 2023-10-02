7d3ce41c073e24abbf4e573079858c10a73ddc5f
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2023-10-02 00:44:10 -0500

    Darken the player ship parts so active player ship stands out

--- core/src/main/java/lando/systems/ld54/objects/PlayerShipPart.java
+++ core/src/main/java/lando/systems/ld54/objects/PlayerShipPart.java
@@ -1,5 +1,7 @@
 package lando.systems.ld54.objects;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import lando.systems.ld54.Assets;
 import lando.systems.ld54.screens.GameScreen;
 
@@ -7,8 +9,17 @@ public class PlayerShipPart extends Debris {
 
     public enum Type { nose, cabin, tail }
 
+    private static final Color DIM = new Color(0.5f, 0.5f, 0.5f, 1);
+
     public PlayerShipPart(GameScreen screen, Type type, Assets assets, float x, float y) {
         super(screen, assets.playerShipParts.get(type), x, y);
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+        batch.setColor(DIM);
+        super.draw(batch);
+        batch.setColor(Color.WHITE);
     }
 
 }
