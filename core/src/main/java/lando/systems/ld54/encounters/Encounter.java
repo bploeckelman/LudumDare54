5730e266044c61b5dd7e05ff8bd336113077d532
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2023-10-01 19:07:58 -0500

    Mark Encounter.sector transient so it doesn't cause a gwt problem

--- core/src/main/java/lando/systems/ld54/encounters/Encounter.java
+++ core/src/main/java/lando/systems/ld54/encounters/Encounter.java
@@ -3,7 +3,8 @@ package lando.systems.ld54.encounters;
 import lando.systems.ld54.objects.Sector;
 
 public class Encounter {
-    public Sector sector;
+    public transient Sector sector;
+
     public String title;
     public String text;
     public String imageKey;
